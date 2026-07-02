ce2c7eaa2cb22069826b42b82073d51ecb79280c
hh8658420-crypto <hh8658420@gmail.com>
2026-07-02 23:25:51 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
-// Firebase 联机配置文件。
-// 1. 去 Firebase 控制台创建 Web App 和 Realtime Database。
-// 2. 把你的 firebaseConfig 粘贴到下面。填好后所有玩家进入同一个 room 参数即可同步。
 window.BACCARAT_FIREBASE_CONFIG = {
-  apiKey: "填写你的 apiKey",
-  authDomain: "填写你的项目.firebaseapp.com",
-  databaseURL: "填写你的 Realtime Database URL",
-  projectId: "填写你的 projectId",
-  appId: "填写你的 appId"
+  apiKey: "AIzaSyDijXLWvqRg-ea3Hyg4vCASl05SfoQ-L7I",
+  authDomain: "bjlfwq.firebaseapp.com",
+  databaseURL: "https://bjlfwq-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "bjlfwq",
+  storageBucket: "bjlfwq.firebasestorage.app",
+  messagingSenderId: "666510175833",
+  appId: "1:666510175833:web:1d1c5593bc0066289e4b48"
 };
+
 window.BACCARAT_ROOM_ID = "VIP001";
