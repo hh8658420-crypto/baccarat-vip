0d93441b1af622381c80309ea813ecd62054a4ee
hh8658420-crypto <hh8658420@gmail.com>
2026-07-02 23:24:54 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase 联机配置文件。
+// 1. 去 Firebase 控制台创建 Web App 和 Realtime Database。
+// 2. 把你的 firebaseConfig 粘贴到下面。填好后所有玩家进入同一个 room 参数即可同步。
+window.BACCARAT_FIREBASE_CONFIG = {
+  apiKey: "填写你的 apiKey",
+  authDomain: "填写你的项目.firebaseapp.com",
+  databaseURL: "填写你的 Realtime Database URL",
+  projectId: "填写你的 projectId",
+  appId: "填写你的 appId"
+};
+window.BACCARAT_ROOM_ID = "VIP001";
